b82a0346a52df55e5d55fae98c3a8e3fe1c28730
AlvaroG98 <201700377@ujmd.edu.sv>
2020-08-25 19:23:31 -0600

    primer commit

---
+++ src/proyectodeconsola_prc/pkg4/main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectodeconsola_prc.pkg4;
+
+/**
+ *
+ * @author GUILLERMO
+ */
+public class main {
+    
+}
