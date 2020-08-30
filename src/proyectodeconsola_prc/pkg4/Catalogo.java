0a4edd4913a175255d436c6fafd1bbd1195e6045
“jdquinteros” <201600781@ujmd.edu.sv>
2020-08-30 17:47:46 -0600

    Creacion del listado de catalogo

---
+++ src/proyectodeconsola_prc/pkg4/Catalogo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectodeconsola_prc.pkg4;
+
+/**
+ *
+ * @author Josue Quinteros
+ */
+public class Catalogo {
+    
+    /* INVENTARIO
+    1. PAN (20 U.)
+    2. LECHE (15 U.)
+    3. MARGARINA (25 U.)
+    4. HUEVOS (30 U.)
+    5. PAPEL HIGIENICO (30 U.)
+    6. JABON (10 U.)
+    7. SHAMPOO (15 U.)
+    8. DESODORANTE (15 U.)
+    9. FRIJOLES NATURAS(25 U.)
+    10.ACEITE (15 U.)
+    11.QUESO CHEDAR (20 U.)
+    12.CREMA VAQUITA (25 U.)
+    13.CHURRITO DIANA (30 U.)
+    14.COCA COLA (35 U.)
+    15.PEPSI (25 U.)
+    16.NARANJAS (15 U.)
+    17.SANDIA (8 U.)
+    19.MELON (10 U.)
+    20.MANZANA (18 U.)
+    */
+    
+}
