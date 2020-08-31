e7a9b659c1771732f58b2ebbe225828fb6424965
“jdquinteros” <201600781@ujmd.edu.sv>
2020-08-30 19:07:54 -0600

    Creacion de clase catalogo

--- src/proyectodeconsola_prc/pkg4/Catalogo.java
+++ src/proyectodeconsola_prc/pkg4/Catalogo.java
@@ -10,6 +10,38 @@ package proyectodeconsola_prc.pkg4;
  * @author Josue Quinteros
  */
 public class Catalogo {
+        private Scanner teclado2;
+        
+    public void imprimir(){
+        
+        teclado2 = new Scanner(System.in);
+       
+            System.out.println("Inventario");
+            System.out.println(" 1. PAN (20 U.)");
+            System.out.println("2. LECHE (15 U.)");
+            System.out.println("3. MARGARINA (25 U.)");
+            System.out.println("4. HUEVOS (30 U.)");
+            System.out.println("5. PAPEL HIGIENICO (30 U.)");
+            System.out.println("6. JABON (10 U.)");
+            System.out.println("7. SHAMPOO (15 U.)");
+            System.out.println("8. DESODORANTE (15 U.)");
+            System.out.println("9. FRIJOLES NATURAS(25 U.)");
+            System.out.println("10.ACEITE (15 U.)");
+            System.out.println("11.QUESO CHEDAR (20 U.)");
+            System.out.println("12.CREMA VAQUITA (25 U.)");
+            System.out.println("13.CHURRITO DIANA (30 U.)");
+            System.out.println("14.COCA COLA (35 U.)");
+            System.out.println("15.PEPSI (25 U.)");
+            System.out.println("16.NARANJAS (15 U.)");
+            System.out.println("17.SANDIA (8 U.)");
+            System.out.println("18.MANDARINA (12 U.)");
+            System.out.println("19.MELON (10 U.)");
+            System.out.println("20.MANZANA (18 U.)");
+            
+        
+        }
+    
+    }
     
     /* INVENTARIO
     1. PAN (20 U.)
@@ -29,6 +61,7 @@ public class Catalogo {
     15.PEPSI (25 U.)
     16.NARANJAS (15 U.)
     17.SANDIA (8 U.)
+    18.MANDARINA (12 U.)
     19.MELON (10 U.)
     20.MANZANA (18 U.)
     */
